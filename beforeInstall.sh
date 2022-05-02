6393fffae7616e9a54d6c9639d7625fd51150db9
JD4912 <58825633+JD4912@users.noreply.github.com>
2022-05-02 17:03:50 +0530

    Update beforeInstall.sh

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -3,5 +3,5 @@ rm -rf /home/ubuntu/rails
 git clone https://github.com/JD4912/test_project.git /home/ubuntu/rails
 cd /home/ubuntu/rails && rm Gemfile.lock
 gem install bundle
-# bundle install
+bundle install
 
