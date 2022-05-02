8ed3de8a539e3d5125bb08744bf5a1c463af9045
JD4912 <58825633+JD4912@users.noreply.github.com>
2022-05-02 19:37:17 +0530

    Update beforeInstall.sh

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -5,7 +5,9 @@ rm -rf /home/ubuntu/rails
 git clone https://github.com/JD4912/test_project.git /home/ubuntu/rails
 cd /home/ubuntu/rails && rm Gemfile.lock
 ls
-RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production bundle exec rake assets:clobber
-RAILS_ENV=production bundle exec rake assets:precompile
-RAILS_ENV=production bundle install --path vendor/bundle
+which rails
+# RAILS_ENV=production bundle exec rake db:migrate
+# RAILS_ENV=production bundle exec rake assets:clobber
+# RAILS_ENV=production bundle exec rake assets:precompile
+# RAILS_ENV=production bundle install --path vendor/bundle
+bundle install
