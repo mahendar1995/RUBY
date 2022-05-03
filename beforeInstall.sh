419af9beda933683d9eca285bb7f4da5be9d7413
JD4912 <58825633+JD4912@users.noreply.github.com>
2022-05-03 12:54:58 +0530

    Update beforeInstall.sh

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo su - 
-git clone https://github.com/rbenv/rbenv-vars.git ~/.rbenv/plugins/rbenv-vars
-exec $SHELL
-rbenv install 2.7.2
-rbenv global 2.7.2
-gem install bundler
+# git clone https://github.com/rbenv/rbenv-vars.git ~/.rbenv/plugins/rbenv-vars
+# exec $SHELL
+# rbenv install 2.7.2
+# rbenv global 2.7.2
+# gem install bundler
 
 sudo rm -rf /home/ubuntu/rails
 sudo git clone https://github.com/JD4912/test_project.git /home/ubuntu/rails
